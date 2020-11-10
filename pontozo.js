69a7851ce4510f26af730556a117638c20de33ae
Bencezs1 <zsbence2003@gmail.com>
2020-11-10 15:40:42 +0100

    js változtatás

--- pontozo.js
+++ pontozo.js
@@ -5,27 +5,26 @@ function check(){
     var d = document.pontozo.Halo.value;
     var e = document.pontozo.Prog.value;
     var f = document.pontozo.Web.value,
-    var ossz = 0;
-    ossz = a + b + c + d + e + f;
+    var ossz = a + b + c + d + e + f;
 
     var pont;
     if (ossz>=168) {
         pont = 5
     }
-    if (ossz>=126) {
-        pont = 4
+    else if (ossz>=126) {
+        pont = 4;
     }
-    if (ossz>=84) {
-        pont = 3
+    else if (ossz>=84) {
+        pont = 3;
     }
-    if (ossz>=52) {
-        pont = 2
+    else if (ossz>=52) {
+        pont = 2;
     }
-    if (ossz<52) {
-        pont = 1
-    }
+   else {
+       pomt = 1;
+   }
 
     document.getElementById("kuldes").style.visibility = "visible";
-    document.getElementById("mennyi").innerHTML = "Neked " + ossz + " pontod van.";
+   document.getElementById("mennyi").innerHTML = ossz;
 
 }
